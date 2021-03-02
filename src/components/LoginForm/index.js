dc0081b2bf92c0f6d229d258056d2b4144b71e85
dembowmaker1204 <ptb1204@hotmail.com>
2021-03-02 17:12:19 -0500

    Login Form

---
+++ src/components/LoginForm/index.js
@@ -0,0 +1,38 @@
+function LoginForm({email, password, handleSubmit, handleChange}){
+
+    return(
+
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="email">E-mail:</label>
+            <input
+                type="email"
+                id="email"
+                name="email"
+                value={email}
+                onChange={handleChange}
+            />
+            <label htmlFor="password">Password:</label>
+            <input 
+                type="password"
+                id="password"
+                name="password"
+                value={password}
+                onChange={handleChange}
+            />
+            <button 
+                type="submit"
+            >
+                Sign In
+            </button>
+             <button 
+                type="button"
+             >
+                Create User
+            </button>
+            <a href="#">Forgot Password?</a>
+
+        </form>
+    )
+}
+
+export default LoginForm;
