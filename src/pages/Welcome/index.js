0d167ceb8c42c42034a811b4179a2fd4f9ac58f9
Miguel Garcia <me.garcian@uniandes.edu.co>
2021-03-17 13:10:35 -0500

    fixed issue with double indentation

--- src/pages/Welcome/index.js
+++ src/pages/Welcome/index.js
@@ -1,22 +1,22 @@
 import React from 'react'
 import {
-    SectionWelcome,
-    SectionBRPub, 
+  SectionWelcome,
+  SectionBRPub, 
 } from './styles'
 
 
 function Welcome(props) {
-    const user = props.location.state ? props.location.state.email : props.match.params.id
+  const user = props.location.state ? props.location.state.email : props.match.params.id
 
-    return(
-        <SectionWelcome>
-            <a href="/" className="Logout">Logout</a>
-            <h1>Welcome {user}</h1>
-            <SectionBRPub>
-                <a href="/buy-rent" className="BuyRent">Buy/Rent</a>
-                <a href="/publish" className="Publish">Publish</a>
-            </SectionBRPub>
-        </SectionWelcome>
-    )
+  return(
+    <SectionWelcome>
+      <a href="/" className="Logout">Logout</a>
+      <h1>Welcome {user}</h1>
+      <SectionBRPub>
+        <a href="/buy-rent" className="BuyRent">Buy/Rent</a>
+        <a href="/publish" className="Publish">Publish</a>
+      </SectionBRPub>
+    </SectionWelcome>
+  )
 }
 export default Welcome
--- src/pages/Welcome/styles.js
+++ src/pages/Welcome/styles.js
@@ -2,54 +2,54 @@ import styled from 'styled-components'
 import background from '../../media/pictures/welcome.jpg'
 
 export const SectionWelcome = styled.section`
-    height: 100vh;
-    position: relative;
-    background-color: black;
-    /* background-position: center;
-    background-repeat: no-repeat;
-    background-size: cover; */
+  height: 100vh;
+  position: relative;
+  background-color: black;
+  /* background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover; */
 
-    & .Logout{
-        position: absolute;
-        right: 0;   
-        text-decoration: none;
-        font-size: 1.3em;
-    }
+  & .Logout{
+    position: absolute;
+    right: 0;   
+    text-decoration: none;
+    font-size: 1.3em;
+  }
 
-    & h1{
-        /* margin-top: 5rem; */
-        margin: 0;
-        text-align: center; 
-        color: green;
-    }
+  & h1{
+    /* margin-top: 5rem; */
+    margin: 0;
+    text-align: center; 
+    color: green;
+  }
 `
 export const SectionBRPub = styled.section`
+  margin: auto;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  width: 200px;
+  height: 200px;
+  border: 2px solid #000;
+  border-radius: 10px;
+  box-shadow: 5px 5px lightgreen;
+  margin-block: 6rem;
+  background-image: url(${background});
+
+  & .BuyRent, & .Publish{
+    border: 1px solid green;
+    width: 100px;
     margin: auto;
-    text-align: center;
-    display: flex;
-    flex-direction: column;
-    width: 200px;
-    height: 200px;
-    border: 2px solid #000;
-    border-radius: 10px;
-    box-shadow: 5px 5px lightgreen;
-    margin-block: 6rem;
-    background-image: url(${background});
+    margin-bottom: 1rem;
+    height: 29px;
+    border-radius: 5px;
+    text-decoration: none;
+    background-color: green;
+    color: white;
+    box-shadow: 2px 2px lightgreen;
+  }
 
-    & .BuyRent, & .Publish{
-        border: 1px solid green;
-        width: 100px;
-        margin: auto;
-        margin-bottom: 1rem;
-        height: 29px;
-        border-radius: 5px;
-        text-decoration: none;
-        background-color: green;
-        color: white;
-        box-shadow: 2px 2px lightgreen;
-    }
-
-    & .Publish{
-        margin-bottom:auto;
-    }
+  & .Publish{
+    margin-bottom:auto;
+  }
 `
